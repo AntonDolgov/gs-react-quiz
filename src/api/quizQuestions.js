1a570557ed90c0c43bb183d10d8549b049bf5515
AntonDolgov <dlgvnt@gmail.com>
2018-09-06 23:34:56 +0300

    init

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,41 @@
+
+
+var quizQuestions = [
+  {
+      question: "What is your spirit animal ? ",
+      answers: [
+          {
+              type: "Stark",
+              content: "Wolf"
+              
+          },
+          {
+              type: "Lannister",
+              content: "Lion"
+          },
+          {
+              type: "Targaryen",
+              content: "Dragon"
+          }
+      ]
+  },
+  {
+      question: "What do you value most ? ",
+      answers: [
+          {
+              type: "Stark",
+              content: "Loyalty"
+          },
+          {
+              type: "Lannister",
+              content: "Money"
+          },
+          {
+              type: "Targaryen",
+              content: "Power"
+          }
+      ]
+  }
+];
+ 
+export default quizQuestions;
